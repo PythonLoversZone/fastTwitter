94f7489e3161d77afdbc0f00b4fd5e4b144a2da1
xiaomo <xiaomo@xiaomo.info>
2018-01-27 15:46:15 +0800

    refactoring

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ import logging.config
 import tweepy
 from tweepy import TweepError
 
-logging.config.fileConfig('logging_config.ini')
+base = 'config/'
+follower_url = base + 'follow.txt'
+log_config_url = base + 'logging_config.ini'
+api_config = base + 'cfg.json'
+logging.config.fileConfig(log_config_url)
 
 # create logger
 logger = logging.getLogger(__name__)
@@ -35,7 +39,7 @@ def get_api(cfg):
 # 读取文件中需要follow的名字列表
 def get_name_list():
     name_list = dict()
-    with open('follow.txt', 'r', encoding='utf-8') as f:
+    with open(follower_url, 'r', encoding='utf-8') as f:
         for line in f:
             names = line.split(" ")
             if len(names) < 2:
@@ -48,7 +52,7 @@ def get_name_list():
 
 # 读取配置文件
 def get_config():
-    with open('cfg.json') as f:
+    with open(api_config) as f:
         setting = json.load(f)
         return setting
 
